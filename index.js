f655c165367e84a0ffb82aa45e4e4b604917aa68
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-09-04 14:48:12 -0400

    added a password check to validate both password inputs to make sure they are the same. Red border goes on them if not same and green if they are

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("test");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+
+confirmPassword.addEventListener("input", checkPassword);
+password.addEventListener("input", checkPassword);
+
+function checkPassword() {
+  let enteredPass = password.value;
+  let enteredConfirmPass = confirmPassword.value;
+  if (enteredPass === enteredConfirmPass) {
+    if (enteredPass === "" || enteredConfirmPass === "") {
+      console.log(enteredPass);
+      password.classList.remove("valid");
+      confirmPassword.classList.remove("valid");
+      password.classList.remove("error");
+      confirmPassword.classList.remove("error");
+      return;
+    }
+    password.classList.remove("error");
+    confirmPassword.classList.remove("error");
+    password.classList.add("valid");
+    confirmPassword.classList.add("valid");
+    return;
+  }
+
+  password.classList.remove("valid");
+  confirmPassword.classList.remove("valid");
+  password.classList.add("error");
+  confirmPassword.classList.add("error");
+}
